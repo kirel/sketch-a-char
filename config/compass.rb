8a4593f6fdfa5c6ca9fefe79d68559726e006b9e
Daniel Kirsch <danishkirel@gmail.com>
2011-01-24 14:49:45 +0100

    initial

---
+++ config/compass.rb
@@ -0,0 +1,13 @@
+# This configuration file works with both the Compass command line tool and within Rails.
+# Require any additional compass plugins here.
+project_type = :rails
+project_path = Compass::AppIntegration::Rails.root
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "javascripts"
+environment = Compass::AppIntegration::Rails.env
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DetexifyRails::Application.config.secret_token = '5cfff3e1794fc318bd56b368b94329d3d3cc0e00a4766d9ddebfd223cb50a1e4ec3dd963c1818970d51c2d3e86af7e6855304dd1143f9411a1ccf03bd3e1a372'
