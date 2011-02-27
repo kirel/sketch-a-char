d8eb1f839253afcd9668b3d2c6aa7a421b5822a4
Daniel Kirsch <danishkirel@gmail.com>
2011-02-27 23:31:49 +0100

    bounding box

--- public/javascripts/strokes.js
+++ public/javascripts/strokes.js
@@ -1,3 +1,4 @@
+// uses sylvester and Underscore
 var Strokes = (function () {
   return {
     sim: function(fst, snd) {
@@ -42,6 +43,29 @@ var Strokes = (function () {
       // }
       // return res.concat(_.rest(stroke));
       return stroke;
+    },
+    // returns a Matrix where the cols are bottom-left and top-right
+    boundingbox: function(stroke) {
+      var l = _(stroke).first().e(1);
+      var b = _(stroke).first().e(2);
+      var r = _(stroke).first().e(1);
+      var t = _(stroke).first().e(2);
+      var lbrt = _(stroke).chain().rest().reduce(function(lbrt, v) {
+        var l = v.e(1);
+        var b = v.e(2);
+        var r = v.e(1);
+        var t = v.e(2);
+        var lMin = lbrt[0];
+        var bMin = lbrt[1];
+        var rMax = lbrt[2];
+        var tMax = lbrt[3];
+        return [Math.min(l, lMin), Math.min(b, bMin), Math.max(r, rMax), Math.max(t, tMax)];
+      }, [l,b,r,t]).value();
+      var lMin = lbrt[0];
+      var bMin = lbrt[1];
+      var rMax = lbrt[2];
+      var tMax = lbrt[3];
+      return $M([[lbrt[0], lbrt[2]], [lbrt[1], lbrt[3]]]);
     }
   };
 })();
