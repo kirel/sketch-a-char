9ecb9b52a5de810423c36dd0def9c506940a0848
Daniel Kirsch <danishkirel@gmail.com>
2011-03-20 00:05:20 +0100

    fixes env variable for newer omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_filter :authenticate
   def create
-    auth = request.env['rack.auth']
+    auth = request.env['omniauth.auth']
     unless @auth = Authorization.find_from_hash(auth)
       # Create a new user or add an auth to existing user, depending on
       # whether there is already a user signed in.
