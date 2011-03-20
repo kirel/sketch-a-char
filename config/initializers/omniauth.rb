74925653d4326c120c854345cdd211ea23d15634
Daniel Kirsch <danishkirel@gmail.com>
2011-03-20 20:52:59 +0100

    facebook fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '0g7zY1OfGPPpndDfomhCg', 'Hn7N05pcXx3rfO2AhDCF8762GEJWR83dJQDcAaN2TQI'
-  provider :facebook, '89b2a0fcbf6b1cba3c7c49a2ad66aa1d', 'd15e841943d1c7f6769e77eabd70a1eag', scope: ''
+  provider :facebook, '185398324838487', 'd15e841943d1c7f6769e77eabd70a1eagst', scope: ''
 end
