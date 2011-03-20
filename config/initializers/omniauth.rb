573a9dbba61db0af31cb81508ba2015eebe9accc
Daniel Kirsch <danishkirel@gmail.com>
2011-03-20 20:34:33 +0100

    facebook and logout

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, '0g7zY1OfGPPpndDfomhCg', 'Hn7N05pcXx3rfO2AhDCF8762GEJWR83dJQDcAaN2TQI'
-  provider :facebook, '89b2a0fcbf6b1cba3c7c49a2ad66aa1d', 'd15e841943d1c7f6769e77eabd70a1eag'
-  # provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :facebook, '89b2a0fcbf6b1cba3c7c49a2ad66aa1d', 'd15e841943d1c7f6769e77eabd70a1eag', scope: ''
 end
