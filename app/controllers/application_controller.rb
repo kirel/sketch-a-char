b45c8c4a419cbd10f5f96b96f186f6d8b8278cf5
Daniel Kirsch <danishkirel@gmail.com>
2011-03-20 17:48:44 +0100

    No basic auth in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate
   protected
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "foo" && password == "bar"
-    end if Rails.env.production?
-  end
-  
+    
   def current_user
-    @current_user ||= User.find_by_id(session[:user_id])
+    if id = session[:user_id]
+      @current_user ||= User.find_by_id(id)
+    end
   end
 
   def signed_in?
