fc657af1a01aed437a9cf3cfdf0ff85aab599a4a
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-10-03 19:35:23 +0200

    Move OAuth config somewhere else

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '0g7zY1OfGPPpndDfomhCg', 'Hn7N05pcXx3rfO2AhDCF8762GEJWR83dJQDcAaN2TQI'
-  provider :facebook, '185398324838487', 'd15e841943d1c7f6769e77eabd70a1eagst', scope: ''
-  provider :open_id, OpenID::Store::Filesystem.new('./tmp')  
+  provider :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: ''
+  provider :open_id, OpenID::Store::Filesystem.new('./tmp')
 end
