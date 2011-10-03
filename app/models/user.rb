3037aef5ca7ae4639c52b064bedd561db585e989
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-10-03 19:10:30 +0200

    Whitespace and dev mode admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,23 @@ class User < ActiveRecord::Base
   has_friendly_id :name, :use_slug => true
   acts_as_voter
   has_karma :samples, as: :submitter
-  
+
   def self.create_from_hash!(hash)
     create!(:name => hash['user_info']['name'])
   end
-  
+
   def identity?
     authorizations.any?
   end
-  
+
   def admin?
-    superuser? # || Rails.env.development? # TODO allow based on karma!
+    superuser? || Rails.env.development? # TODO allow based on karma!
   end
-  
+
   def update_karma_cache!
     self.karma_cache = self.karma
     save! validate: false
   end
-  
+
   scope :top, order('users.karma_cache DESC')
-  
 end
