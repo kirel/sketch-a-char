ec273375432b61086d862b50b8dea18f4ff396bb
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-10-05 23:50:26 +0200

    Use fb and twitter oauth only if env valiables set

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
-  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: ''
+  provider :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET'] if ENV['TWITTER_APP_ID']
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: '' if ENV['FB_APP_ID']
   provider :open_id, OpenID::Store::Filesystem.new('./tmp')
 end
