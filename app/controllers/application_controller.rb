ed853e7de22af1b62121b0f3248f63de4715daf4
Daniel Kirsch <daniel.kirsch@zweitag.de>
2011-12-06 11:00:28 +0100

    Whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   protected
-    
+
   def current_user
     if id = session[:user_id]
       @current_user ||= User.find_by_id(id)
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def signed_in?
     !!current_user
   end
-  
+
   def admin?
     signed_in? && current_user.admin?
   end
@@ -21,25 +21,25 @@ class ApplicationController < ActionController::Base
   def login!(user)
     @current_user = user.tap { session[:user_id] = user.id }
   end
-  
+
   def logout!
     nil.tap { session.delete :user_id }
   end
-  
+
   def require_current_user
     redirect_to login_path unless signed_in?
   end
-  
+
   def require_admin
     permission_denied unless admin?
   end
-  
+
   def permission_denied
     render :file => "public/401.html", :status => :unauthorized
   end
-  
+
   def cache!
     response.headers['Cache-Control'] = 'public, max-age=86400' # cache for a day
   end
-  
+
 end
